245e4637476117a287f66f2adfc94a241b3e7bf4
Theo Carnavos <tcarnavos@gmail.com>
2016-02-03 15:08:14 -0600

    Added main XHR load function with nested loops for dog data

---
+++ javascripts/food.js
@@ -0,0 +1,59 @@
+// Declare new XHR variable which will eventually be used for the dogfood JSON file
+var dogRequest = new XMLHttpRequest();
+
+// Testing with dog data only first
+function executeThisCodeAfterFileIsLoaded () {
+	console.log("File loaded");
+	console.log(Date.now());
+
+	// Parse the response text as JSON
+	var data = JSON.parse(this.responseText);
+	console.log("data", data);
+
+	// Declare element variables
+	var dogContainer = document.getElementById("dog");
+	console.log("dogContainer: ", dogContainer);
+
+	var productData = "";
+	var currentProduct;
+
+	// Loop through the dog_brands array
+	for (var i = 0; i < data.dog_brands.length; i++) {
+		currentProduct = data.dog_brands[i];
+
+		productData += `<div class="brandBlock">`;
+		productData += `<p>Brand Name: ${currentProduct.name}</p>`;
+		console.log("name: ", currentProduct.name);
+		console.log("types: ", currentProduct.types);
+		// Loop through the "types" array in dog_brands
+		for (var j = 0; j < currentProduct.types.length; j++) {
+			var currentTypes = currentProduct.types[j];
+			productData += `<div class="productBlock">`;
+			productData += `Product Type: ${currentTypes.type}`;
+			console.log("currentTypes.type: ", currentTypes.type);
+			console.log("volumes: ");
+			// Loop through the "volumes" array in types and display "name" & "price"
+			for (var k = 0; k < currentTypes.volumes.length; k++) {
+				var currentVolumes = currentTypes.volumes[k];
+				productData += `<div class="volumeBlock">`;
+				productData += `Name: ${currentVolumes.name}`;
+				productData += `Price: $${currentVolumes.price}`;
+				productData += `</div>`; // Closing volumeBlock
+				console.log("currentVolumes Name: ", currentVolumes.name);
+				console.log("currentVolumes Price: ", currentVolumes.price);
+			}
+		productData += `</div>`; //Closing productBlock
+		console.log("productData: ", productData);
+		}
+	productData += `</div>`; //Closing brandBlock
+	}
+	// Fill dog container with html string
+	dogContainer.innerHTML = productData;
+};
+
+dogRequest.addEventListener("load", executeThisCodeAfterFileIsLoaded);
+
+dogRequest.open("GET", "dog.json");
+dogRequest.send();
+
+
